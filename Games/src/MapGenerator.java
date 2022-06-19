eccc2bc58046010e67ce5fb0e50e8799961d5137
Antoan <stoikovantoan@gmail.com>
2022-06-19 18:02:57 +0300

    Commiting Bricket game

--- Games/src/MapGenerator.java
+++ Games/src/MapGenerator.java
@@ -17,6 +17,7 @@ public class MapGenerator {
     	brickWidth = 540 / col;
     	brickHeight = 150 / row;
     }
+    // drawing bricks
     public void draw(Graphics2D g) {
     	for (int i = 0; i < map.length; i++) {
     		for(int j = 0; j < map[0].length; j++) {
